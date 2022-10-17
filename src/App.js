a3579edbee4af43f860972038f23e99044c839de
maryamkaramad <maryamkaramad1373@gmail.com>
2022-10-17 20:25:28 +0330

    add2 mycontact base

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import './App.css';
 import {RouterProvider} from "react-router-dom";
 import router from './routs/routs';
-import PhoneContextProvider from './context/contaxt'
 function App() {
   return (
-    <PhoneContextProvider><RouterProvider router={router}/></PhoneContextProvider>
+    <RouterProvider router={router}/>
 
    
   );
